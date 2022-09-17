b2761bbe0bb30dc4a5f77460bc16b4b62dadbe8a
dik94 <79519936+pahlawanDeveloper@users.noreply.github.com>
2022-09-17 17:28:51 +0800

    buat fungsi di promise

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const gabungData = [...await promiseTheaterIXX(), ...await promiseTheaterVGC()];
+  let hitung = 0;
+  gabungData.forEach(dataHasil => {
+    if (dataHasil.hasil === emosi) {
+      hitung++;
+    }
+  });
+  return hitung;
+};
 
 module.exports = {
   promiseOutput,
